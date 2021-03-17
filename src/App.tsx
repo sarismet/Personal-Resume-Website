b62a2ad075998caed20f979ff0e73ede2c73388d
sarismet <sarismet2825@gmail.com>
2021-03-17 17:01:39 +0300

    first version of the site

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>
+          My WebSite
+        </h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
