2e35f28187d9970cae57f05846208652c41cc7b3
İsmet Sarı <49066101+sarismet@users.noreply.github.com>
2021-03-26 17:48:50 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var app = express();
-
+const fs = require('fs')
 
 app.use(express.static('.'));
 
@@ -9,7 +9,11 @@ app.get('/', function (req, res) {
 });
 
 app.listen(8081, () => {
-
-    console.log("Example app listening at port 3000",)
-
+    console.log("Example app listening at port 8081")
 })
+
+app.post('/resume', function(req, res, next) {
+    var data = fs.readFileSync('/mnt/c/Users/ismet/Desktop/Personal-Resume-Website/files/resume.pdf');
+    res.contentType("application/pdf");
+    res.send(data);
+});
